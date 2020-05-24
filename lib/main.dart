f78600735d4cc4ccf662c7fa2f8377595dee37a5
Jonathan Sequeira <sequeira.s.jonathan@gmail.com>
2020-05-23 22:50:40 -0400

    converted to stateful widget and introduced state

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,53 @@
+//Packages
 import 'package:flutter/material.dart';
 
-void main () => runApp(MyApp());
+//Widgets
+import './question.dart';
+import './answer.dart';
 
-class MyApp extends StatelessWidget{
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  var _title = 'My First App';
+  var _questionIndex = 0;
+  var questions = [
+    {'questionText': 'What\'s your favorite color?', 'answers':['black', 'Red', 'Green', 'White'],},
+    {'questionText': 'What\'s your favorite animal?', 'answers':['Rabbit', 'Snake', 'Elephant', 'Lion'],},
+    {'questionText': 'Who is your favorite instructor?', 'answers':['Max','Max','Max','Max'],},
+];
+
+  void _answerQuestion() {
+    if (_questionIndex >= questions.length - 1) {
+      setState(() {
+        _questionIndex = 0;
+      });
+    } else {
+      setState(() {
+        _questionIndex += 1;
+      });
+    }
+    print(_questionIndex);
+  }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Hello"),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text(_title),
+          ),
+          body: Column(children: [
+            Question(questions[_questionIndex]['questionText']),
+            //... operator extracts each list indivudually instead of creating nested lists
+            ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
+              return Answer(_answerQuestion,answer);
+            }).toList(),
+          ])),
     );
   }
 }
